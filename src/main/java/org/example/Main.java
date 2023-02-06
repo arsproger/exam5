e964eabda387c5184a67569baebdac9ed95d4871
Arsen <arsproger@gmail.com>
2023-02-06 18:13:59 +0600

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        App app = new App();
+        app.authorize();
+        app.register();
+    }
+}
